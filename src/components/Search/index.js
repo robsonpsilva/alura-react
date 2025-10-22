c4a2ce48066d75fed4d81c2dea1592daf772c69f
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-10-22 14:55:38 -0300

    modified:   src/App.js 	new file:   src/components/Search/index.js

---
+++ src/components/Search/index.js
@@ -0,0 +1,12 @@
+function Search() {
+    return (
+        <section>
+            <h2>Ja sabe onde começar</h2>
+            <h3>Encontre seu livro em nossa estante.</h3>
+            <input type="text" placeholder="Escreva sua próxima leitura"/>
+        </section>
+        
+    );
+}
+
+export default Search;
